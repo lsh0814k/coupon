e5b1786283320a54e69f5df5fecde75d41d20855
lee <lsg0814k@gmail.com>
2024-05-02 10:38:16 +0900

    동시성 이슈 발생으로 인한 synchronized 블록 추가

--- coupon-api/src/main/java/com/fem/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/fem/couponapi/service/CouponIssueRequestService.java
@@ -13,7 +13,9 @@ public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        synchronized (this) {
+            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        }
         log.info("쿠폰 발급 완료. couponId : {}, userId: {}", requestDto.couponId(), requestDto.userId());
     }
 }
