157aa8ec81079316525e3d98b9497c9edfc315d3
lee <lsg0814k@gmail.com>
2024-05-02 16:44:16 +0900

    redisTemplate 을 이용한 repository

---
+++ coupon-core/src/main/java/com/fem/couponcore/repository/redis/CouponRedisRepository.java
@@ -0,0 +1,27 @@
+package com.fem.couponcore.repository.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CouponRedisRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Long sAdd(String key, String value) {
+        return redisTemplate.opsForSet().add(key, value);
+    }
+
+    public Long sCard(String key) {
+        return redisTemplate.opsForSet().size(key);
+    }
+
+    public Boolean sIsMember(String key, String value) {
+        return redisTemplate.opsForSet().isMember(key, value);
+    }
+
+    public Long rPush(String key, String value) {
+        return redisTemplate.opsForList().rightPush(key, value);
+    }
+}
