2f2e15a08ba5821f33fd22f26a0c24797d0b475f
lee <lsg0814k@gmail.com>
2024-05-02 11:23:54 +0900

    빌드 오류에 따른 테스트 properties 추가

--- coupon-api/src/test/java/com/fem/couponapi/CouponApiApplicationTests.java
+++ coupon-api/src/test/java/com/fem/couponapi/CouponApiApplicationTests.java
@@ -2,8 +2,13 @@ package com.fem.couponapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.name=application-core")
 class CouponApiApplicationTests {
 
     @Test
--- coupon-consumer/src/test/java/com/fem/couponconsumer/CouponConsumerApplicationTests.java
+++ coupon-consumer/src/test/java/com/fem/couponconsumer/CouponConsumerApplicationTests.java
@@ -2,8 +2,12 @@ package com.fem.couponconsumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.name=application-core")
 class CouponConsumerApplicationTests {
 
     @Test
