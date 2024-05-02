bf8a5567e4ce3cee9d3f5c494923d0868d38ab58
lee <lsg0814k@gmail.com>
2024-05-02 18:31:00 +0900

    redis cache 설정

--- coupon-core/src/main/java/com/fem/couponcore/CouponCoreConfiguration.java
+++ coupon-core/src/main/java/com/fem/couponcore/CouponCoreConfiguration.java
@@ -1,9 +1,11 @@
 package com.fem.couponcore;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @ComponentScan
 @EnableJpaAuditing
 @EnableAutoConfiguration
